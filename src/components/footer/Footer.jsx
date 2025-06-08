cb9dfc8a3771be53583886b1ca8bd7b0d3a8c094
Hasnain Arif <hasnainarif571@gmail.com>
2025-06-08 14:25:05 +0500

    chore: add navbar, footer, and layout structure with theme provider setup

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="lama blog" width={50} height={50} />
+          <h1 className={styles.logoText}>theScribbly</h1>
+        </div>
+        <p className={styles.desc}>
+          theScribbly is a place where ideas come to life — from personal
+          stories to deep dives into tech, art, and culture. Join our community
+          of writers and readers who believe in the power of thoughtful content
+          and authentic voices. Stay curious, stay inspired.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/tiktok.png" alt="" width={18} height={18} />
+          <Image src="/youtube.png" alt="" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
