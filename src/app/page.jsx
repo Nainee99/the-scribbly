84afeda9c0715ad952fe5931228cb09a031b0506
Hasnain Arif <hasnainarif571@gmail.com>
2025-06-09 23:05:41 +0500

    feat(ui/api): fix import of Menu component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,7 @@ import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
 
+
 export default function Home({ searchParams }) {
   const page = parseInt(searchParams.page) || 1;
 
