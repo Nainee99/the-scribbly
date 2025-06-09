2af17f3c1eee0f068fc0bf3ca24a834a530f28da
Hasnain Arif <hasnainarif571@gmail.com>
2025-06-09 19:37:19 +0500

    feat(api): implement comment APIs (create, fetch, list) and integrate with backend

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -8,7 +8,7 @@ export const GET = async (req) => {
   const page = searchParams.get("page");
   const cat = searchParams.get("cat");
 
-  const POST_PER_PAGE = 2;
+  const POST_PER_PAGE = 5;
 
   const query = {
     take: POST_PER_PAGE,
@@ -56,8 +56,6 @@ export const POST = async (req) => {
     );
   }
 };
-
-
 
 // GET RANDOM POSTS
 export const GET_RANDOM = async (req) => {
